9d0bd8f4e1d5b9082a3654df094d67a9d9f06832
lungcollapser <milesterrell1337@gmail.com>
2024-08-22 21:59:50 -0500

    JOIN JOIN CUH

--- Beginner - Parks_and_Rec_Create_db.sql
+++ Beginner - Parks_and_Rec_Create_db.sql
@@ -3,4 +3,9 @@ FROM employee_salary
 WHERE salary BETWEEN 25000 AND 50000;
 
 SELECT * 
-FROM parks_departments
+FROM parks_departments;
+
+SELECT *
+FROM employee_demographics
+JOIN employee_salary
+	ON employee_demographics.employee_id = employee_salary.employee_id
