f5fecb447f79f64f32e85618a14fdc6bd3aeef38
lungcollapser <milesterrell1337@gmail.com>
2024-08-26 20:41:09 -0500

    dndsqueelk

--- Beginner - Parks_and_Rec_Create_db.sql
+++ Beginner - Parks_and_Rec_Create_db.sql
@@ -1,11 +1,2 @@
-SELECT *
-FROM employee_salary
-WHERE salary BETWEEN 25000 AND 50000;
-
 SELECT * 
-FROM parks_departments;
-
-SELECT *
-FROM employee_demographics
-JOIN employee_salary
-	ON employee_demographics.employee_id = employee_salary.employee_id
+FROM classes
